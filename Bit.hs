ed8e8e650a4eb0066befca5f12f3229b59fd6374
Matthew Naylor <mattfn@gmail.com>
2018-01-18 22:21:03 +0000

    More primitives

--- Bit.hs
+++ Bit.hs
@@ -2,7 +2,7 @@
 {-# OPTIONS_GHC -fno-cse -fno-full-laziness #-}
 
 -- For type-level naturals
-{-# LANGUAGE DataKinds, KindSignatures, TypeOperators#-}
+{-# LANGUAGE DataKinds, KindSignatures, TypeOperators, TypeFamilies, GADTs #-}
 
 module Blarney.Bit
   ( Bit      -- "Bit n" is a bit vector of n bits
@@ -16,11 +16,20 @@ module Blarney.Bit
   , (.>.)    -- Greater than
   , (.<=.)   -- Less than or equal
   , (.>=.)   -- Greater than or equal
-  , (#)      -- Concatenate
+  , reg      -- Register
+  , regEn    -- Register with enable
+  , (#)      -- Concatenation
+  , bit      -- Bit selection
+  , (!)      -- Sub-range selection
+  -- TODO: div, mod, shift left, shift right,
+  -- upper, lower, zeroExtend, signExtend
   ) where
 
 -- For type-level literals
 import GHC.TypeLits
+
+-- For type-level indices
+import TypeIndex
 
 -- For Observable Sharing
 import Data.IORef
@@ -56,11 +65,8 @@ data Pin =
 -- Phantom type for a pin, capturing the bit width
 newtype Bit (n :: Nat) = Bit { unBit :: Pin }
 
--- Type class synonym
-class KnownNat n => N n
-
 -- Determine bit width
-width :: N n => Bit n -> Int
+width :: KnownNat n => Bit n -> Int
 width a = fromInteger (natVal a)
 
 -- Helper function for creating instance of a primitive component
@@ -91,69 +97,69 @@ primInst1 prim params ins outWidth =
   head (primInst prim params ins [outWidth])
 
 -- Unary arithmetic primitive
-primArith1 :: N n => PrimName -> [Param] -> Bit n -> Bit n
+primArith1 :: KnownNat n => PrimName -> [Param] -> Bit n -> Bit n
 primArith1 prim params a = Bit (primInst1 prim params [unBit a] (width a))
 
 -- Binary arithmetic primitive
-primArith2 :: N n => PrimName -> [Param] -> Bit n -> Bit n -> Bit n
+primArith2 :: KnownNat n => PrimName -> [Param] -> Bit n -> Bit n -> Bit n
 primArith2 prim params a b =
   Bit (primInst1 prim params [unBit a, unBit b] (width a))
 
 -- Bitwise invert
-inv :: N n => Bit n -> Bit n
+inv :: KnownNat n => Bit n -> Bit n
 inv = primArith1 "~" []
 
 -- Bitwise and
 infixl 7 .&.
-(.&.) :: N n => Bit n -> Bit n -> Bit n
+(.&.) :: KnownNat n => Bit n -> Bit n -> Bit n
 (.&.) = primArith2 "&" []
 
 -- Bitwise or
 infixl 5 .|.
-(.|.) :: N n => Bit n -> Bit n -> Bit n
+(.|.) :: KnownNat n => Bit n -> Bit n -> Bit n
 (.|.) = primArith2 "|" []
 
 -- Bitwise xor
 infixl 6 .^.
-(.^.) :: N n => Bit n -> Bit n -> Bit n
+(.^.) :: KnownNat n => Bit n -> Bit n -> Bit n
 (.^.) = primArith2 "^" []
 
 -- Mux
-(?) :: N n => Bit 1 -> (Bit n, Bit n) -> Bit n
+(?) :: KnownNat n => Bit 1 -> (Bit n, Bit n) -> Bit n
 sel ? (a, b) =
   Bit (primInst1 "?" [] [unBit sel, unBit a, unBit b] (width a))
 
 -- Binary comparison helper
-primCmp2 :: N n => PrimName -> [Param] -> Bit n -> Bit n -> Bit 1
+primCmp2 :: KnownNat n => PrimName -> [Param] -> Bit n -> Bit n -> Bit 1
 primCmp2 prim params a b =
   Bit (primInst1 prim params [unBit a, unBit b] 1)
 
 -- Equality
-(.==.) :: N n => Bit n -> Bit n -> Bit 1
+(.==.) :: KnownNat n => Bit n -> Bit n -> Bit 1
 (.==.) = primCmp2 "==" []
 
 -- Disequality
-(.!=.) :: N n => Bit n -> Bit n -> Bit 1
+(.!=.) :: KnownNat n => Bit n -> Bit n -> Bit 1
 (.!=.) = primCmp2 "!=" []
 
 -- Less than
-(.<.) :: N n => Bit n -> Bit n -> Bit 1
+(.<.) :: KnownNat n => Bit n -> Bit n -> Bit 1
 (.<.) = primCmp2 "<" []
 
 -- Less than or equal
-(.<=.) :: N n => Bit n -> Bit n -> Bit 1
+(.<=.) :: KnownNat n => Bit n -> Bit n -> Bit 1
 (.<=.) = primCmp2 "<=" []
 
 -- Greater than
-(.>.) :: N n => Bit n -> Bit n -> Bit 1
+(.>.) :: KnownNat n => Bit n -> Bit n -> Bit 1
 (.>.) = primCmp2 ">" []
 
 -- Greater than or equal
-(.>=.) :: N n => Bit n -> Bit n -> Bit 1
+(.>=.) :: KnownNat n => Bit n -> Bit n -> Bit 1
 (.>=.) = primCmp2 ">=" []
 
 -- Arithmetic
-instance N n => Num (Bit n) where
+instance KnownNat n => Num (Bit n) where
   (+)           = primArith2 "+" []
   (-)           = primArith2 "-" []
   (*)           = primArith2 "*" []
@@ -166,9 +172,28 @@ instance N n => Num (Bit n) where
      w          = width result
 
 -- Register
-reg :: N n => Integer -> Bit n -> Bit n
+reg :: KnownNat n => Integer -> Bit n -> Bit n
 reg init = primArith1 "reg" ["init" :-> show init]
 
--- Concatenate
-(#) :: (N n, N m) => Bit n -> Bit m -> Bit (n+m)
+-- Register with enable
+regEn :: KnownNat n => Integer -> Bit 1 -> Bit n -> Bit n
+regEn init en a = 
+  Bit (primInst1 "regEn" ["init" :-> show init]
+        [unBit en, unBit a] (width a))
+
+-- Concatenation
+(#) :: (KnownNat n, KnownNat m) => Bit n -> Bit m -> Bit (n+m)
 a # b = Bit (primInst1 "#" [] [unBit a, unBit b] (width a + width b))
+
+-- Bit selection
+bit :: (KnownNat i, (i+1) <= n) => I i -> Bit n -> Bit 1
+bit i a = 
+  Bit (primInst1 "bit" ["index" :-> show (natVal i)] [unBit a] 1)
+
+-- Sub-range selection
+(!) :: (KnownNat hi, KnownNat lo, (hi+1) <= n, lo <= hi, (lo+m)~hi) =>
+       Bit n -> (I hi, I lo) -> Bit m
+a ! (hi, lo) =
+  Bit (primInst1 "range" ["high" :-> show (natVal hi),
+                          "low"  :-> show (natVal lo)]
+      [unBit a] (fromInteger (natVal hi - natVal lo)))
