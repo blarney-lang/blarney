b37c3fd4bc5a78b50cf83ceb0fc048c1fa42c7a6
Matthew Naylor <mattfn@gmail.com>
2019-03-21 17:19:19 +0000

    Fun with RV32I

---
+++ Examples/Stone/Pipeline.hs
@@ -0,0 +1,187 @@
+module Pipeline where
+
+-- 4-stage pipeline, parameterised by the ISA.
+-- We assume the ISA is a 32-bit 3-operand register machine.
+
+import Blarney
+import Blarney.RAM
+import Blarney.BitScan
+
+-- Instructions
+type Instr = Bit 32
+
+-- Register identifiers
+type RegId = Bit 5
+
+-- Instruction memory size
+type InstrAddr = Bit 16
+
+-- Pipeline configuration
+data Config =
+  Config {
+    -- Get source register A
+    srcA :: Instr -> RegId
+    -- Get source register B
+  , srcB :: Instr -> RegId
+    -- Get destination register
+  , dst :: Instr -> RegId
+    -- Dispatch rules for pre-execute stage
+  , preExecRules :: State -> [Instr -> Action ()]
+    -- Dispatch rules for execute stage
+  , execRules :: State -> [Instr -> Action ()]
+  }
+
+-- Pipeline state, visisble to the ISA
+data State =
+  State {
+    -- Source operands
+    opA :: Bit 32
+  , opB :: Bit 32
+    -- Current PC
+  , getPC :: Bit 32
+    -- Write a new PC
+  , setPC :: Bit 32 -> Action ()
+    -- Write the instruction result
+  , result :: Wire (Bit 32)
+  }
+
+-- Pipeline
+makeCPUPipeline :: Config -> Module ()
+makeCPUPipeline c = do
+  -- Instruction memory
+  instrMem :: RAM InstrAddr Instr <- makeRAMInit "prog.hex"
+
+  -- Two block RAMs allows two operands to be read,
+  -- and one result to be written, on every cycle
+  regFileA :: RAM RegId (Bit 32) <- makeDualRAMForward 0
+  regFileB :: RAM RegId (Bit 32) <- makeDualRAMForward 0
+
+  -- Instruction register
+  instr :: Reg Instr <- makeReg dontCare
+
+  -- Instruction operand registers
+  regA :: Reg (Bit 32) <- makeReg dontCare
+  regB :: Reg (Bit 32) <- makeReg dontCare
+
+  -- Wire used to overidge the update to the PC,
+  -- in case of a branch instruction
+  pcNext :: Wire (Bit 32) <- makeWire dontCare
+
+  -- By default, increment the PC
+  pc :: Reg (Bit 32) <- makeReg 0
+  always do
+    if (pcNext.active)
+      then pc <== pcNext.val
+      else pc <== pc.val + 4
+
+  -- Result of the execute stage
+  resultWire :: Wire (Bit 32) <- makeWire dontCare
+
+  -- Cycle counter
+  count :: Reg (Bit 32) <- makeReg 0
+  always (count <== count.val + 1)
+
+  -- Program counters for pipeline stages 2 and 3
+  pc1 :: Reg (Bit 32) <- makeReg 0
+  pc2 :: Reg (Bit 32) <- makeReg 0
+  pc3 :: Reg (Bit 32) <- makeReg 0
+
+  -- Instruction registers for pipeline stages 2 and 3
+  instr2 :: Reg Instr <- makeReg 0
+  instr3 :: Reg Instr <- makeReg 0
+
+  -- Triggers for pipeline stages 2 and 3
+  go2 :: Reg (Bit 1) <- makeDReg 0
+  go3 :: Reg (Bit 1) <- makeDReg 0
+
+  always do
+    -- Stage 0: Instruction Fetch
+    -- ==========================
+
+    let reqPC = pcNext.active ? (pcNext.val, pc.val)
+
+    -- Index the instruction memory
+    let (instrAddr :: Bit 30, bottom :: Bit 2) = split reqPC
+    load instrMem (lower instrAddr)
+
+    -- Always trigger stage 1, except on first cycle
+    let go1 :: Bit 1 = reg 0 1
+
+    pc1 <== reqPC
+
+    -- Stage 1: Operand Fetch
+    -- ======================
+
+    -- Trigger stage 2, except on pipeline flush
+    when go1 do
+      when (pcNext.active.inv) do
+        go2 <== 1
+
+    -- Fetch operands
+    load regFileA (c.srcA $ instrMem.out)
+    load regFileB (c.srcB $ instrMem.out)
+
+    -- Latch instruction and PC for next stage
+    instr2 <== instrMem.out
+    pc2 <== pc1.val
+
+    -- Stage 2: Latch Operands
+    -- =======================
+  
+    -- Register forwarding logic
+    let forward rS other =
+         (resultWire.active .&. ((c.dst $ instr3.val) .==. instr2.val.rS)) ?
+         (resultWire.val, other)
+
+    -- Latch operands
+    regA <== forward (c.srcA) (regFileA.out)
+    regB <== forward (c.srcB) (regFileB.out)
+
+    -- State for pre-execute stage
+    let state = State {
+            opA    = regFileA.out
+          , opB    = regFileB.out
+          , getPC  = pc2.val
+          , setPC  = error "setPC not allowed in pre-execute"
+          , result = error "result wire can't be used in pre-execute"
+          }
+
+    -- Trigger stage 3, except on pipeline flush
+    when (pcNext.active.inv) do
+      match (instr2.val) (c.preExecRules $ state)
+      go3 <== go2.val
+
+    -- Latch instruction and PC for next stage
+    instr3 <== instr2.val
+    pc3 <== pc2.val
+
+    -- Stage 3: Execute
+    -- ================
+
+    -- State for execute stage
+    let state = State {
+            opA    = regA.val
+          , opB    = regB.val
+          , getPC  = pc3.val
+          , setPC  = \pcNew -> pcNext <== pcNew
+          , result = resultWire
+          }
+
+    -- Instruction dispatch
+    when (go3.val) do
+      match (instr3.val) (c.execRules $ state)
+
+    if (go3.val) .==. 1
+      then do
+        display "C " (count.val) " pc=" (pc3.val) " instr=" (instr3.val)
+        return ()
+      else do
+        display "_ " (count.val) " pc=" (pc3.val) " instr=" (instr3.val)
+        return ()
+
+    -- Writeback
+    when (resultWire.active) do
+      let rd = dst c (instr3.val)
+      store regFileA rd (resultWire.val)
+      store regFileB rd (resultWire.val)
+      display (count.val) ": rf[" rd "] := " (resultWire.val)
---
+++ Examples/Stone/RV32I.hs
@@ -0,0 +1,40 @@
+import Blarney
+import Blarney.RAM
+import Blarney.BitScan
+import Pipeline
+
+add :: State -> Action ()
+add s = s.result <== s.opA + s.opB
+
+addi :: State -> Bit 12 -> Action ()
+addi s imm = s.result <== s.opA + signExtend imm
+
+bne :: State -> Bit 12 -> Action ()
+bne s imm = do
+  let fullImm = imm # (0 :: Bit 1)
+  when (s.opA .!=. s.opB) do
+    s.setPC $ s.getPC + signExtend fullImm
+
+makeRV32I :: Module ()
+makeRV32I = do
+  let execute s =
+        [ "imm[11:0] _rs1[4:0] 000 _rd[4:0] 0010011" ==> addi s
+        , "0000000 _rs2[4:0] _rs1[4:0] 000 _rd[4:0] 0110011" ==> add s
+        , "imm[11] imm[9:4] _rs2[4:0] _rs1[4:0] 001" ++
+          "imm[3:0] imm[10] 1100011" ==> bne s
+        ]
+
+  makeCPUPipeline $
+    Config {
+      srcA = range @19 @15
+    , srcB = range @24 @20
+    , dst  = range @11 @7
+    , preExecRules = \s -> []
+    , execRules = execute
+    }
+
+-- Main function
+main :: IO ()
+main = do
+  writeVerilogTop makeRV32I "top" "RV32I-Verilog/"
+  return ()
