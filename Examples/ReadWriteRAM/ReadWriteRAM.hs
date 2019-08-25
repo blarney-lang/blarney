403c34a224f5706a41236e8ad39b569ab7117627
Dillon Huff <dillonhuff@gmail.com>
2019-08-24 22:27:19 -0700

    More minor changes to ram

--- Examples/ReadWriteRAM/ReadWriteRAM.hs
+++ Examples/ReadWriteRAM/ReadWriteRAM.hs
@@ -7,25 +7,20 @@ top = do
   -- RAM
   ram :: RAM (Bit 8) (Bit 128) <- makeRAM
 
+  globalTime :: Reg (Bit 32) <- makeReg 0
   i :: Reg (Bit 8) <- makeReg 0
 
-  let testSeq = Seq [While (i.val .<. 100) (Do [store ram (i.val) (1 .<<. i.val), i <== i.val + 1]), Do [i <== 0], While (i.val .<. 100) (Do [load ram (val i), display "ram[0x%02x]" (val i) " = 0x%024x" (out ram), i <== i.val + 1])]
-  --Seq [
-    --While (i.val .<. 100) (
-        --Do [
-        --store ram (i.val) (1 .<<. i.val),
-        --i <== i.val + 1
-        --] 
-        --),
-    --Do [ i <== 0 ],
-    --While (i.val .<. 100) (
-        --Do [
-        --load ram (val i),
-        --display "ram[0x%02x]" (val i) " = 0x%024" (out ram),
-        --i <== val i + 1
-        --]
-        --)
-  --]
+  always do
+    globalTime <== globalTime.val + 1
+    display "global time = 0dx" (globalTime.val)
+
+  let testSeq =
+        Seq [
+          While (i.val .<. 100) (
+            Do [store ram (i.val) (1 .<<. i.val), i <== i.val + 1]),
+            Do [i <== 0],
+            While (i.val .<. 100) (Do [load ram (val i), display "ram[0x%02x]" (val i) " = 0x%024x" (out ram), i <== i.val + 1])
+            ]
 
   done <- run (reg 1 0) testSeq
 
