9a634b0cf4106f69b489d8c5eda23786cffd7071
Dillon Huff <dillonhuff@gmail.com>
2019-08-25 01:18:37 -0700

    Experimenting with pipeline syntax

--- Examples/ReadWriteRAM/PipelineTest.hs
+++ Examples/ReadWriteRAM/PipelineTest.hs
@@ -8,6 +8,17 @@ top = do
 
   i :: Reg (Bit 8) <- makeReg 0
 
+  -- What Id like:
+  -- WhileP (i.val .<. 20) (Do [NB $ store ram (i.val), NB $ i <== i.val + 1, iterate])
+  -- Q: When iterate is placed somewhere does that mean "iterate on the next cycle? or does it mean iterate in the current cycle?"
+  -- A: In the current cycle, but when would you want to do that?
+
+  -- WhileP (i.val .<. 20) (NB [store ram (i.val), Do [i <== i.val + 1, iterate]]) 
+  -- In this framing the datapath operations and the index calculator are different
+
+  -- WhileP (i.val .<. 20) (1) (Do [store ram (i.val), i <== i.val + 1])
+  -- The problem with this implementation is that i is not set until after the store, but it
+  -- needs to be set in the first cycle
   let testSeq =
         Seq [
           While (i.val .<. 20) (Do [store ram (i.val) (zeroExtend $ i.val), i <== i.val + 1]),
