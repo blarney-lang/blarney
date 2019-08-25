18f6cf2d2e934b31a13b11215211e44d0dddf893
Dillon Huff <dillonhuff@gmail.com>
2019-08-25 13:20:07 -0700

    Needed to make res a register

--- Examples/VectorAdd/VectorAdd.hs
+++ Examples/VectorAdd/VectorAdd.hs
@@ -15,25 +15,41 @@ top = do
 
   i:: Reg (Bit 8) <- makeReg 0
 
-  res :: Wire (Bit 32) <- makeWire dontCare
+  res :: Reg (Bit 32) <- makeReg dontCare
   let testSeq =
         Seq [
           While (i.val .<. 20) (Do [
+          display "storing %02d" (i.val),
           store vecA (i.val) (zeroExtend $ i.val),
           store vecB (i.val) (zeroExtend $ i.val),
           i <== i.val + 1]),
-          Do [i <== 0],
 
+          Do [i <== 0],
 
 
           While (i.val .<. 20) (
             Seq [
               parA [load vecA (i.val), load vecB (i.val)],
-              res <== (out vecA) .+. (out vecB),
+
+              Par [act $ display "a val = %02d" (out vecA), act $ display "b val = %02d" (out vecB), act $ res <== (out vecA) +  (out vecB)],
+              act $ display "res = %02d" (res.val ), 
               act $ store vecC (i.val) (val res),
+              act $ display "storing %02d" (res.val),
               act $ i <== i.val + 1
             ]
+          ),
+  
+          Do [i <== 0],
+          act $ display "C values after un-pipelined add",
+
+          While (i.val .<. 20) (
+            Do [
+              load vecC (i.val),
+              display "C[%02d]" (i.val) " = %02d" (out vecC),
+              i <== i.val + 1
+            ]
           )
+
         ]
 
   done <- run (reg 1 0) testSeq
