ae6ce912547affc7f8deef061b7d8a9910e230d6
gameboo <alexandre.joannou@gmail.com>
2020-05-26 15:50:16 +0100

    A few more optimisations in constant folding

--- Haskell/Blarney/Core/Net.hs
+++ Haskell/Blarney/Core/Net.hs
@@ -166,24 +166,88 @@ propagateNames nl = do
 pattern Lit i <- InputTree (Const _ i) []
 -- | Helper to evaluate constant Net
 evalConstNet :: Net -> (Net, Bool)
+-- Add
+evalConstNet n@Net{ netPrim = Add w, netInputs = [Lit 0, x] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
+evalConstNet n@Net{ netPrim = Add w, netInputs = [x, Lit 0] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
 evalConstNet n@Net{ netPrim = Add w, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const w (a0 + a1), netInputs = [] }, True)
+-- Sub -- TODO 0 - x could use a new negate primitive ?
+evalConstNet n@Net{ netPrim = Sub w, netInputs = [x, Lit 0] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
 evalConstNet n@Net{ netPrim = Sub w, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const w (a0 - a1), netInputs = [] }, True)
+-- Mul
+evalConstNet n@Net{ netPrim = Mul w, netInputs = [Lit 0, _] } =
+  (n { netPrim = Const w 0, netInputs = [] }, True)
+evalConstNet n@Net{ netPrim = Mul w, netInputs = [_, Lit 0] } =
+  (n { netPrim = Const w 0, netInputs = [] }, True)
+evalConstNet n@Net{ netPrim = Mul w, netInputs = [Lit 1, x] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
+evalConstNet n@Net{ netPrim = Mul w, netInputs = [x, Lit 1] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
 evalConstNet n@Net{ netPrim = Mul w, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const w (a0 * a1), netInputs = [] }, True)
+-- Div
+evalConstNet n@Net{ netPrim = Div w, netInputs = [_, Lit 0] } =
+  error $ "Division by constant 0 detected"
+evalConstNet n@Net{ netPrim = Div w, netInputs = [Lit 0, _] } =
+  (n { netPrim = Const w 0, netInputs = [] }, True)
+evalConstNet n@Net{ netPrim = Div w, netInputs = [x, Lit 1] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
 evalConstNet n@Net{ netPrim = Div w, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const w (a0 `div` a1), netInputs = [] }, True)
+-- Mod
+evalConstNet n@Net{ netPrim = Mod w, netInputs = [_, Lit 0] } =
+  error $ "Modulo by constant 0 detected"
+evalConstNet n@Net{ netPrim = Mod w, netInputs = [Lit 0, _] } =
+  (n { netPrim = Const w 0, netInputs = [] }, True)
+evalConstNet n@Net{ netPrim = Mod w, netInputs = [x, Lit 1] } =
+  (n { netPrim = Const w 0, netInputs = [] }, True)
 evalConstNet n@Net{ netPrim = Mod w, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const w (a0 `mod` a1), netInputs = [] }, True)
+-- Not
 evalConstNet n@Net{ netPrim = Not w, netInputs = [Lit a0] } =
   (n { netPrim = Const w ((2^w-1) `B.xor` a0), netInputs = [] }, True)
+-- And
+evalConstNet n@Net{ netPrim = And w, netInputs = [Lit 0, x] } =
+  (n { netPrim = Const w 0, netInputs = [] }, True)
+evalConstNet n@Net{ netPrim = And w, netInputs = [x, Lit 0] } =
+  (n { netPrim = Const w 0, netInputs = [] }, True)
+evalConstNet n@Net{ netPrim = And w, netInputs = [Lit v, x] } | v == 2^w-1 =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
+evalConstNet n@Net{ netPrim = And w, netInputs = [x, Lit v] } | v == 2^w-1 =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
 evalConstNet n@Net{ netPrim = And w, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const w (a0 B..&. a1), netInputs = [] }, True)
+-- Or
+evalConstNet n@Net{ netPrim = Or w, netInputs = [Lit 0, x] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
+evalConstNet n@Net{ netPrim = Or w, netInputs = [x, Lit 0] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
+evalConstNet n@Net{ netPrim = Or w, netInputs = [Lit v, x] } | v == 2^w-1 =
+  (n { netPrim = Const w (2^w-1), netInputs = [] }, True)
+evalConstNet n@Net{ netPrim = Or w, netInputs = [x, Lit v] } | v == 2^w-1 =
+  (n { netPrim = Const w (2^w-1), netInputs = [] }, True)
 evalConstNet n@Net{ netPrim = Or w, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const w (a0 B..|. a1), netInputs = [] }, True)
+-- Xor
+evalConstNet n@Net{ netPrim = Xor w, netInputs = [Lit 0, x] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
+evalConstNet n@Net{ netPrim = Xor w, netInputs = [x, Lit 0] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
+evalConstNet n@Net{ netPrim = Xor w, netInputs = [Lit v, x] } | v == 2^w-1 =
+  (n { netPrim = Not w, netInputs = [x] }, True)
+evalConstNet n@Net{ netPrim = Xor w, netInputs = [x, Lit v] } | v == 2^w-1 =
+  (n { netPrim = Not w, netInputs = [x] }, True)
 evalConstNet n@Net{ netPrim = Xor w, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const w (a0 `B.xor` a1), netInputs = [] }, True)
+-- ShiftLeft
+evalConstNet n@Net{ netPrim = ShiftLeft _ w, netInputs = [Lit 0, _] } =
+  (n { netPrim = Const w 0, netInputs = [] }, True)
+evalConstNet n@Net{ netPrim = ShiftLeft _ w, netInputs = [x, Lit 0] } =
+  (n { netPrim = Identity w, netInputs = [x] }, True)
 evalConstNet n@Net{ netPrim = ShiftLeft _ w, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const w (a0 `B.shiftL` fromInteger (a1))
      , netInputs = [] }, True)
@@ -195,6 +259,7 @@ evalConstNet n@Net{ netPrim = ShiftLeft _ w, netInputs = [Lit a0, Lit a1] } =
 --  n { netPrim   = Const w (inV a0 `B.shiftR` fromInteger (inV a1))
 --    , netInputs = [] }
 -- TODO XXX FORCE UNSIGNED FOR NON ARITHMETIC SHIFTS
+-- Comparisons
 evalConstNet n@Net{ netPrim = Equal _, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const 1 (if a0 == a1 then 1 else 0), netInputs = [] }, True)
 evalConstNet n@Net{ netPrim = NotEqual _, netInputs = [Lit a0, Lit a1] } =
@@ -213,18 +278,21 @@ evalConstNet n@Net{ netPrim = ReplicateBit w, netInputs = [Lit a0] } =
 --  n { netPrim   = Const w1 (if inV a0 > 0 then inV a0 else --TODO)
 --    , netInputs = [] }
 -- TODO XXX
+-- SelectBits
 evalConstNet n@Net{ netPrim = SelectBits w hi lo, netInputs = [Lit a0] } =
   (n { netPrim   = Const (hi-lo+1) ((a0 `B.shiftR` lo) B..&. (2^(hi-lo+1)-1))
     , netInputs = [] }, True)
+-- Concat
 evalConstNet n@Net{ netPrim = Concat w0 w1, netInputs = [Lit a0, Lit a1] } =
   (n { netPrim = Const (w0+w1) ((a0 `B.shiftL` w1) B..|. a1)
      , netInputs = [] }, True)
+-- Mux
 evalConstNet n@Net{ netPrim = Mux _ w, netInputs = (Lit s):xs } =
-  case xs !! fromInteger s of
-    Lit x -> (n { netPrim = Const w x, netInputs = [] }, True)
-    _     -> (n, False)
+  (n { netPrim = Identity w, netInputs = [xs !! fromInteger s] }, True)
+-- Identity
 evalConstNet n@Net{ netPrim = Identity w, netInputs = [Lit a0] } =
   (n { netPrim   = Const w a0, netInputs = [] }, True)
+-- Fall-through case, no change
 evalConstNet n = (n, False)
 
 -- | Constant folding pass
