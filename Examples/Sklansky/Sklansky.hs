5bf35d0498b879c133ee0898c3f9dd3366a8948c
Matthew Naylor <mn416@cam.ac.uk>
2020-12-07 19:00:18 +0000

    Subjective refinements of SMT2 examples

--- Examples/Sklansky/Sklansky.hs
+++ Examples/Sklansky/Sklansky.hs
@@ -1,6 +1,7 @@
 import Blarney
 import qualified Blarney.Vector as V
 
+-- | Sklanksy parallel prefix network (assumes power-of-2 list size)
 sklansky :: (a -> a -> a) -> [a] -> [a]
 sklansky op [x] = [x]
 sklansky op xs = ys' ++ map (last ys' `op`) zs'
@@ -8,21 +9,24 @@ sklansky op xs = ys' ++ map (last ys' `op`) zs'
     (ys, zs) = halve xs
     ys' = sklansky op ys
     zs' = sklansky op zs
+
+-- | Split a list into two halves
 halve :: [a] -> ([a], [a])
 halve xs = (take n xs, drop n xs)
   where n = length xs `div` 2
 
---sklanskyM :: KnownNat m => (Bit n -> Bit n -> Bit n) -> V.Vec m (Bit n) -> Module (V.Vec m (Bit n))
---sklanskyM op ins = return $ V.fromList $ sklansky op (V.toList ins)
-
-isSklanskyGood :: forall n m. (Bit n -> Bit n -> Bit n) -> V.Vec m (Bit n) -> Module (Bit 1)
-isSklanskyGood op ins = return $ andList $ zipWith (.==.) res0 res1
-  where res0 = sklansky op (V.toList ins)
-        res1 = scanl1 op (V.toList ins)
+-- | Does parallel scan equal sequential scan for given operator?
+prop_scan :: forall n m.
+     (Bit n -> Bit n -> Bit n)
+  -> V.Vec m (Bit n) -> Module (Bit 1)
+prop_scan op ins = return equal
+  where
+    res0 = sklansky op (V.toList ins)
+    res1 = scanl1 op (V.toList ins)
+    equal = andList (zipWith (.==.) res0 res1)
 
 main :: IO ()
 main = do
   print (scanl1 (+) [1..16])
   print (sklansky (+) [1..16])
-  --writeVerilogModule (sklanskyM @4 @3) "sklansky4_3" "Sklansky-Verilog/"
-  writeSMT2Script (isSklanskyGood @4 @4 (+)) "goodSklansky4_4" "Sklansky-SMT2/"
+  writeSMT2Script (prop_scan @4 @4 (+)) "goodSklansky4_4" "Sklansky-SMT2/"
