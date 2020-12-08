0ed9e7c2c4a93f677ecb2c9f485e4a3b2a237572
Alexandre Joannou <aj443@cam.ac.uk>
2020-12-08 13:43:53 +0000

    Refine SMT Adder and Sklansky examples

--- Examples/Adder/Adder.hs
+++ Examples/Adder/Adder.hs
@@ -1,3 +1,5 @@
+import System.Directory
+
 import Blarney
 
 -- | Half adder taking two bits and returning a tuple with the carry bit first
@@ -32,13 +34,28 @@ doom_adder :: KnownNat n => Bit n -> Bit n -> Bit n
 doom_adder x y = fromBitList $ listAdder 1 (toBitList x) (toBitList y)
 
 -- | Adder property
-prop_add :: KnownNat n => (Bit n -> Bit n -> Bit n)
-                       -> Bit n -> Bit n -> Module (Bit 1)
-prop_add adder x y = return $ adder x y .==. x + y
+prop_add :: KnownNat n => (Bit n -> Bit n -> Bit n) -> Bit n -> Bit n -> Bit 1
+prop_add adder_imp x y = adder_imp x y .==. x + y
 
 main :: IO ()
 main = do
-  writeSMT2Script (prop_add @16 adder) "goodAdder16" "Adder-SMT2/"
-  writeSMT2Script (prop_add @1 doom_adder) "brokenAdder1" "Adder-SMT2/"
-  writeSMT2Script (prop_add @2 doom_adder) "brokenAdder2" "Adder-SMT2/"
-  writeSMT2Script (prop_add @16 doom_adder) "brokenAdder16" "Adder-SMT2/"
+  -- path to script output directory
+  cwd <- getCurrentDirectory
+  let smtDir = cwd ++ "/Adder-SMT2/"
+  -- generate smt2 scripts
+  writeSMT2Script (prop_add @2  adder)      "goodAdder2"    smtDir
+  writeSMT2Script (prop_add @16 adder)      "goodAdder16"   smtDir
+  writeSMT2Script (prop_add @2  doom_adder) "brokenAdder2"  smtDir
+  writeSMT2Script (prop_add @16 doom_adder) "brokenAdder16" smtDir
+  -- helper usage message
+  putStrLn $ "SMT2 scripts generated under " ++ smtDir
+  putStrLn $ "Run an SMT solver such as z3 with an SMT2 script as input:"
+  putStrLn $ "    $ z3 " ++ smtDir ++ "goodAdder2.smt2"
+  putStrLn $ "This should return \"unsat\". Running:"
+  putStrLn $ "    $ z3 " ++ smtDir ++ "brokenAdder2.smt2"
+  putStrLn $ "should return \"sat\". Run:"
+  putStrLn $ "    $ echo \"(get-model)\" >> "
+             ++ smtDir ++ "brokenAdder2.smt2"
+  putStrLn $ "    $ z3 " ++ smtDir ++ "brokenAdder2.smt2"
+  putStrLn $ "to get a set of input assignments for which the tested property"
+             ++ " does not hold."
--- Examples/Sklansky/Sklansky.hs
+++ Examples/Sklansky/Sklansky.hs
@@ -1,5 +1,12 @@
+import System.Directory
+
 import Blarney
 import qualified Blarney.Vector as V
+
+-- | Split a list into two halves
+halve :: [a] -> ([a], [a])
+halve xs = (take n xs, drop n xs)
+  where n = length xs `div` 2
 
 -- | Sklanksy parallel prefix network (assumes power-of-2 list size)
 sklansky :: (a -> a -> a) -> [a] -> [a]
@@ -10,23 +17,44 @@ sklansky op xs = ys' ++ map (last ys' `op`) zs'
     ys' = sklansky op ys
     zs' = sklansky op zs
 
--- | Split a list into two halves
-halve :: [a] -> ([a], [a])
-halve xs = (take n xs, drop n xs)
-  where n = length xs `div` 2
+-- | Broken Sklanksy implementation
+doom_sklansky :: (a -> a -> a) -> [a] -> [a]
+doom_sklansky op [x] = [x]
+doom_sklansky op xs = ys' ++ map (head ys' `op`) zs'
+  where
+    (ys, zs) = halve xs
+    ys' = sklansky op ys
+    zs' = sklansky op zs
 
 -- | Does parallel scan equal sequential scan for given operator?
 prop_scan :: forall n m.
-     (Bit n -> Bit n -> Bit n)
-  -> V.Vec m (Bit n) -> Module (Bit 1)
-prop_scan op ins = return equal
+             ((Bit n -> Bit n -> Bit n) -> [Bit n] -> [Bit n])
+          -> (Bit n -> Bit n -> Bit n)
+          -> V.Vec m (Bit n)
+          -> Bit 1
+prop_scan sklansky_imp op ins = equal
   where
-    res0 = sklansky op (V.toList ins)
+    res0 = sklansky_imp op (V.toList ins)
     res1 = scanl1 op (V.toList ins)
     equal = andList (zipWith (.==.) res0 res1)
 
 main :: IO ()
 main = do
-  print (scanl1 (+) [1..16])
-  print (sklansky (+) [1..16])
-  writeSMT2Script (prop_scan @4 @4 (+)) "goodSklansky4_4" "Sklansky-SMT2/"
+  -- path to script output directory
+  cwd <- getCurrentDirectory
+  let smtDir = cwd ++ "/Sklanksy-SMT2/"
+  -- generate smt2 scripts
+  writeSMT2Script (prop_scan @4 @4 sklansky (+))      "goodSklansky4_4"   smtDir
+  writeSMT2Script (prop_scan @4 @4 doom_sklansky (+)) "brokenSklansky4_4" smtDir
+  -- helper usage message
+  putStrLn $ "SMT2 scripts generated under " ++ smtDir
+  putStrLn $ "Run an SMT solver such as z3 with an SMT2 script as input:"
+  putStrLn $ "    $ z3 " ++ smtDir ++ "goodSklansky4_4.smt2"
+  putStrLn $ "This should return \"unsat\". Running:"
+  putStrLn $ "    $ z3 " ++ smtDir ++ "brokenSklansky4_4.smt2"
+  putStrLn $ "should return \"sat\". Run:"
+  putStrLn $ "    $ echo \"(get-model)\" >> "
+             ++ smtDir ++ "brokenSklansky4_4.smt2"
+  putStrLn $ "    $ z3 " ++ smtDir ++ "brokenSklansky4_4.smt2"
+  putStrLn $ "to get a set of input assignments for which the tested property"
+             ++ " does not hold."
