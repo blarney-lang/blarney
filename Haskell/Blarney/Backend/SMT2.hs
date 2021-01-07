e8e2373cb4a74ad38cd4dcbc36490547086e4486
gameboo <alexandre.joannou@gmail.com>
2021-01-07 01:38:38 +0000

    Added some comments to the SMT2 backend

--- Haskell/Blarney/Backend/SMT2.hs
+++ Haskell/Blarney/Backend/SMT2.hs
@@ -41,14 +41,27 @@ import Blarney.Backend.SMT2.NetlistUtils
 import Blarney.Backend.SMT2.BasicDefinitions
 import Blarney.Misc.ANSIEscapeSequences
 
--- Toplevel API
---------------------------------------------------------------------------------
+-- configuration types
 
+-- | 'Bool' synonym for enabling use of restricted states in induction proofs
 type UseRestrictedStates = Bool
 
-data VerifyDepth = Range Int Int | IncreaseFrom Int deriving Show
+-- | Type to control depth of verification
+data VerifyDepth =
+    -- | Span depths between both 'Int' arguments
+    Range Int Int
+    -- | Span all depths increasingly starting from the 'Int' argument
+  | IncreaseFrom Int
+  deriving Show
+
+-- | Helper function to construct a 'VerifyDepth' describing a fixed unique
+--   depth `n`, i.e. `Range n n`
 fixedDepth :: Int -> VerifyDepth
 fixedDepth n = Range n n
+
+-- | Return the argument 'VerifyDepth' if it is legal, that is the covered
+--   depths are strictly greater than 0, and the higher bound of a 'Range' is
+--   greater than its lower bound. Calls 'error' otherwise.
 legalizeVerifyDepth :: VerifyDepth -> VerifyDepth
 legalizeVerifyDepth x@(Range lo hi)
   | lo > 0 && lo <= hi = x
@@ -60,49 +73,216 @@ legalizeVerifyDepth x@(IncreaseFrom start)
   | start > 0 = x
   | otherwise = error $ "Malformed VerifyDepth" ++ show x ++ ". " ++
                         "Argument " ++ show start ++ " must be > 0. "
-maxVerifyDepth :: VerifyDepth -> Int
-maxVerifyDepth (Range _ x) = x
-maxVerifyDepth (IncreaseFrom x) = x
+
+-- | Return a unique depth rather than a span of values for the provided
+--   'VerifyDepth'.
+--   Return the higher bound of a 'Range' and the starting depth for an
+--   'IncreaseFrom'.
+collapseVerifyDepth :: VerifyDepth -> Int
+collapseVerifyDepth (Range _ x) = x
+collapseVerifyDepth (IncreaseFrom x) = x
+
+-- | Return a pair of depths from a provided 'VerifyDepth'.
+--   If the pair's first element is smaller than its second element, the pair
+--   describe a range of depths to cover. Otherwise, the pair describes an
+--   unbounded set of depths increasing from its first element.
 rangeVerifyDepth :: VerifyDepth -> (Int, Int)
 rangeVerifyDepth (Range x y) = (x, y)
 rangeVerifyDepth (IncreaseFrom x) = (x, 0)
 
-data VerifyMode = Bounded VerifyDepth
-                | Induction VerifyDepth UseRestrictedStates
+-- | Type to specify the kind of verification desired
+data VerifyMode =
+    -- | property holds up to a certain depth
+    Bounded VerifyDepth
+    -- | property holds by induction
+  | Induction VerifyDepth UseRestrictedStates
 
-data UserConf = UserConf { userConfInteractive    :: Bool
-                         , userConfIncreasePeriod :: Int }
+-- | Configuration type for interaction with a human user
+data UserConf = UserConf {
+  -- | Ask the user for confirmation or use default answer
+  userConfInteractive :: Bool
+  -- | For iterative deepening, period at which to ask the user for continuation
+, userConfIncreasePeriod :: Int }
 
+-- | Default 'UserConf':
+-- @
+--    dfltUserConf = UserConf { userConfInteractive    = False
+--                            , userConfIncreasePeriod = 4 }
+-- @
 dfltUserConf = UserConf { userConfInteractive    = False
                         , userConfIncreasePeriod = 4 }
 
-data VerifyConf = VerifyConf { verifyConfMode      :: VerifyMode
-                               -- the following fields only have meaning for
-                               -- the interaction with an SMT solver
-                             , verifyConfSolverCmd :: (String, [String])
-                             , verifyConfUser      :: UserConf
-                             , verifyConfVerbosity :: Int }
+-- | Configuration type for a verification session
+data VerifyConf = VerifyConf {
+  -- | verification type to use
+  verifyConfMode :: VerifyMode
+  -- the following fields only have meaning for interaction with an SMT solver
+  -- | SMT solver command
+, verifyConfSolverCmd :: (String, [String])
+  -- | user configuration to use
+, verifyConfUser      :: UserConf
+  -- | verbosity level for the interactive session
+, verifyConfVerbosity :: Int }
 
+-- | Default 'VerifyConf':
+-- @
+--    dfltVerifyConf = VerifyConf { verifyConfMode      = Bounded (Range 1 1)
+--                                , verifyConfSolverCmd = ("z3", ["-in"])
+--                                , verifyConfUser      = dfltUserConf
+--                                , verifyConfVerbosity = 1 }
+-- @
 dfltVerifyConf = VerifyConf { verifyConfMode      = Bounded (Range 1 1)
                             , verifyConfSolverCmd = ("z3", ["-in"])
                             , verifyConfUser      = dfltUserConf
                             , verifyConfVerbosity = 1 }
 
--- | Convert given blarney 'Netlist' to an SMT2 script
-genSMT2Script :: VerifyConf
-              -> Netlist -- ^ blarney netlist
-              -> String  -- ^ script name
-              -> String  -- ^ output directory
-              -> IO ()
-genSMT2Script VerifyConf{..} nl nm dir =
-  do system ("mkdir -p " ++ dir)
-     withFile fileName WriteMode \h -> do
-       hPutStr h $ renderStyle rStyle smtCode
-  where fileName = dir ++ "/" ++ nm ++ ".smt2"
-        rStyle = Style PageMode 80 1.05
-        smtCode = showAll nl verifyConfMode False
+-- | A context netlist and target root net together with extra list of nets of
+--   interest to identify inputs, registers, etc...
+data Context = Context { ctxtNetlist    :: Netlist
+                       , ctxtInputType  :: String
+                       , ctxtInputIds   :: [InstId]
+                       , ctxtStateType  :: String
+                       , ctxtStateIds   :: [InstId]
+                       , ctxtStateInits :: [(Integer, InputWidth)]
+                       , ctxtRootNet    :: Net
+                       , ctxtTFunName   :: String
+                       , ctxtCFunName   :: String
+                       , ctxtAssertMsg  :: Maybe String }
 
-verifyWithSMT2 :: VerifyConf -> Netlist -> IO ()
+-- | Make a 'Context' from a 'Netlist' and a 'Net' in that 'Netlist'
+mkContext :: Netlist -> Net -> Context
+mkContext nl n = Context { ctxtNetlist    = nl
+                         , ctxtInputType  = inptType
+                         , ctxtInputIds   = inputIds
+                         , ctxtStateType  = stType
+                         , ctxtStateIds   = stateIds
+                         , ctxtStateInits = stateInits
+                         , ctxtRootNet    = n
+                         , ctxtTFunName   = tFun
+                         , ctxtCFunName   = cFun
+                         , ctxtAssertMsg  = msg }
+  where inputIds = [ netInstId n | Just n@Net{netPrim = p} <- elems nl
+                                 , case p of Input _ _ -> True
+                                             _         -> False ]
+        stateElems = [ n | Just n@Net{netPrim = p} <- elems nl
+                         , case p of RegisterEn _ _ -> True
+                                     Register   _ _ -> True
+                                     _              -> False ]
+        stateIds = map netInstId stateElems
+        stateInits = map stateInit stateElems
+        stateInit Net{netPrim=RegisterEn init w} = (init, w)
+        stateInit Net{netPrim=Register   init w} = (init, w)
+        stateInit n = error $ "SMT2 backend error: non state net " ++ show n ++
+                              " encountered where state net was expected"
+        inptType = "Input_" ++ nm
+        stType = "State_" ++ nm
+        tFun = "tFun_" ++ nm
+        cFun = "chain_" ++ tFun
+        (nm, msg) = case netPrim n of
+          Output _      outnm   -> ("output_"++outnm,  Nothing)
+          Assert propmsg -> ("assert_"++propnm, Just propmsg)
+            where propnm = wireName nl propWire
+                  propWire = head . netInputWireIds $ (netInputs n !! 1)
+          _ -> error $ "SMT2 backend error: expected Output or Assert net " ++
+                       "but got " ++ show n
+
+-- Internal helpers
+
+showGeneralDefs :: Bool -> Doc
+showGeneralDefs quiet =
+      shush (char ';' <> text (replicate 79 '-'))
+  $+$ shush (text "; Defining Tuple sorts")
+  $+$ declareTupleTypes [2, 3]
+  $+$ shush (text "; Defining a generic ListX sort")
+  $+$ declareListXType
+  $+$ shush (text "; Defining an \"and\" reduction function for ListX Bool")
+  $+$ defineAndReduce
+  $+$ shush (text "; Defining an \"implies\" reduction function for ListX Bool")
+  $+$ defineImpliesReduce
+  where shush doc = if quiet then empty else doc
+
+showSpecificDefs :: Context -> Bool -> Doc
+showSpecificDefs Context{..} quiet =
+      shush (char ';' <> text (replicate 79 '-'))
+  $+$ shush (text ("; code gen for net" ++ show ctxtRootNet))
+  $+$ shush (char ';' <> text (replicate 79 '-'))
+  $+$ text ("(push)")
+  $+$ shush (char ';' <> text (replicate 79 '-'))
+  $+$ shush (text "; Defining the specific Inputs record type")
+  $+$ declareNLDatatype ctxtNetlist ctxtInputIds ctxtInputType
+  $+$ shush (char ';' <> text (replicate 79 '-'))
+  $+$ shush (text "; Defining the specific State record type")
+  $+$ declareNLDatatype ctxtNetlist ctxtStateIds ctxtStateType
+  $+$ shush (char ';' <> text (replicate 79 '-'))
+  $+$ shush (text "; Defining the specific transition function")
+  $+$ defineNLTransition ctxtNetlist ctxtRootNet
+                         (ctxtInputType, ctxtStateType)
+                         ctxtStateIds
+                         ctxtTFunName
+  $+$ shush (char ';' <> text (replicate 79 '-'))
+  $+$ shush (text "; Defining the specific chaining of the transition function")
+  $+$ defineChainTransition ctxtTFunName
+                            (ctxtInputType, ctxtStateType)
+                            ctxtCFunName
+  where shush doc = if quiet then empty else doc
+
+showSpecificAssert :: Context -> VerifyMode -> Doc
+showSpecificAssert ctxt@Context{..} vMode =
+  check test0 $+$ if doInduction then check test1 else empty
+  where check x = text "(push)" $+$ x $+$ text "(check-sat)" $+$ text "(pop)"
+        test0 = cmnt0 $+$ assertBounded ctxtCFunName
+                                        (ctxtInputType, ctxtStateType)
+                                        ctxtStateInits
+                                        depth
+        test1 = (if restrictStates then rStDefs else empty) $+$ cmnt1
+                $+$ assertInduction ctxtCFunName
+                                    (ctxtInputType, ctxtStateType)
+                                    depth
+                                    restrictStates
+        rStDefs =     text "; Defining helpers for restricted state checking"
+                  $+$ defineDistinctListX ctxtStateType
+        cmnt0 =
+              char ';' <> text (replicate 79 '-')
+          $+$ (if doInduction then
+                  text "; Proof by induction, induction depth =" <+> int depth
+               else text "; Bounded proof for depth =" <+> int depth)
+          $+$ maybe empty (\msg -> text $ "(echo \"" ++ msg ++ "\")")
+                          ctxtAssertMsg
+          $+$ text ("(echo \"" ++ replicate 80 '-' ++ "\")")
+        cmnt1 = char ';' <> text (replicate 79 '-')
+        (depth, doInduction, restrictStates) = case vMode of
+          Bounded vD -> ( collapseVerifyDepth . legalizeVerifyDepth $ vD
+                        , False, False )
+          Induction vD rSt -> ( collapseVerifyDepth . legalizeVerifyDepth $ vD
+                              , True, rSt )
+
+-- | Convert given blarney 'Netlist' to an SMT2 script
+genSMT2Script :: VerifyConf -- ^ configuration for the verification
+              -> Netlist    -- ^ blarney netlist
+              -> String     -- ^ script name
+              -> String     -- ^ output directory
+              -> IO ()
+genSMT2Script VerifyConf{..} nl nm dir = do
+  system ("mkdir -p " ++ dir)
+  withFile fileName WriteMode \h -> do
+    hPutStr h $ renderStyle rStyle smtCode
+  where
+    fileName = dir ++ "/" ++ nm ++ ".smt2"
+    rStyle = Style PageMode 80 1.05
+    rootCtxts = [ mkContext nl n | Just n@Net{netPrim=p} <- elems nl
+                                 , case p of Output _ _ -> True
+                                             Assert _   -> True
+                                             _          -> False ]
+    smtCode =
+          showGeneralDefs False
+      $+$ vcat [ vcat [ showSpecificDefs ctxt False
+                      , showSpecificAssert ctxt verifyConfMode ]
+               | ctxt <- rootCtxts ]
+
+-- | Verify given blarney 'Netlist' predicate using an SMT solver
+verifyWithSMT2 :: VerifyConf -- ^ configuration for the verification
+               -> Netlist    -- ^ blarney property
+               -> IO ()
 verifyWithSMT2 VerifyConf{..} nl =
   withCreateProcess smtP \(Just hIn) (Just hOut) _ _ -> do
     -- set stdin handle to unbuffered for communication with SMT solver
@@ -206,137 +386,3 @@ verifyWithSMT2 VerifyConf{..} nl =
               else when (answer == "y" || answer == "Y") act
               where yes = if dflt then "Y" else "y"
                     no = if dflt then "n" else "N"
-
--- internal helpers specific to blarney netlists
---------------------------------------------------------------------------------
-showGeneralDefs :: Bool -> Doc
-showGeneralDefs quiet =
-      shush (char ';' <> text (replicate 79 '-'))
-  $+$ shush (text "; Defining Tuple sorts")
-  $+$ declareTupleTypes [2, 3]
-  $+$ shush (text "; Defining a generic ListX sort")
-  $+$ declareListXType
-  $+$ shush (text "; Defining an \"and\" reduction function for ListX Bool")
-  $+$ defineAndReduce
-  $+$ shush (text "; Defining an \"implies\" reduction function for ListX Bool")
-  $+$ defineImpliesReduce
-  where shush doc = if quiet then empty else doc
-
-showSpecificDefs :: Context -> Bool -> Doc
-showSpecificDefs Context{..} quiet =
-      shush (char ';' <> text (replicate 79 '-'))
-  $+$ shush (text ("; code gen for net" ++ show ctxtRootNet))
-  $+$ shush (char ';' <> text (replicate 79 '-'))
-  $+$ text ("(push)")
-  $+$ shush (char ';' <> text (replicate 79 '-'))
-  $+$ shush (text "; Defining the specific Inputs record type")
-  $+$ declareNLDatatype ctxtNetlist ctxtInputIds ctxtInputType
-  $+$ shush (char ';' <> text (replicate 79 '-'))
-  $+$ shush (text "; Defining the specific State record type")
-  $+$ declareNLDatatype ctxtNetlist ctxtStateIds ctxtStateType
-  $+$ shush (char ';' <> text (replicate 79 '-'))
-  $+$ shush (text "; Defining the specific transition function")
-  $+$ defineNLTransition ctxtNetlist ctxtRootNet
-                         (ctxtInputType, ctxtStateType)
-                         ctxtStateIds
-                         ctxtTFunName
-  $+$ shush (char ';' <> text (replicate 79 '-'))
-  $+$ shush (text "; Defining the specific chaining of the transition function")
-  $+$ defineChainTransition ctxtTFunName
-                            (ctxtInputType, ctxtStateType)
-                            ctxtCFunName
-  where shush doc = if quiet then empty else doc
-
-showSpecificAssert :: Context -> VerifyMode -> Doc
-showSpecificAssert ctxt@Context{..} vMode =
-  check test0 $+$ if doInduction then check test1 else empty
-  where check x = text "(push)" $+$ x $+$ text "(check-sat)" $+$ text "(pop)"
-        test0 = cmnt0 $+$ assertBounded ctxtCFunName
-                                        (ctxtInputType, ctxtStateType)
-                                        ctxtStateInits
-                                        depth
-        test1 = (if restrictStates then rStDefs else empty) $+$ cmnt1
-                $+$ assertInduction ctxtCFunName
-                                    (ctxtInputType, ctxtStateType)
-                                    depth
-                                    restrictStates
-        rStDefs =     text "; Defining helpers for restricted state checking"
-                  $+$ defineDistinctListX ctxtStateType
-        cmnt0 =
-              char ';' <> text (replicate 79 '-')
-          $+$ (if doInduction then
-                  text "; Proof by induction, induction depth =" <+> int depth
-               else text "; Bounded proof for depth =" <+> int depth)
-          $+$ maybe empty (\msg -> text $ "(echo \"" ++ msg ++ "\")")
-                          ctxtAssertMsg
-          $+$ text ("(echo \"" ++ replicate 80 '-' ++ "\")")
-        cmnt1 = char ';' <> text (replicate 79 '-')
-        (depth, doInduction, restrictStates) = case vMode of
-          Bounded vD -> ( maxVerifyDepth . legalizeVerifyDepth $ vD
-                        , False, False )
-          Induction vD rSt -> ( maxVerifyDepth . legalizeVerifyDepth $ vD
-                              , True, rSt )
-
-showAll :: Netlist -> VerifyMode -> Bool -> Doc
-showAll nl verifyMode quiet =
-  -- general definitions
-      showGeneralDefs quiet
-  -- definitions specific to the netlist
-  $+$ vcat [ showSpecificDefs ctxt quiet | ctxt <- rootCtxts ]
-  -- show the transition function and the induction proof for each netlist root.
-  $+$ vcat [ showSpecificAssert ctxt verifyMode | ctxt <- rootCtxts ]
-  where rootCtxts = [ mkContext nl n | Just n@Net{netPrim=p} <- elems nl
-                                     , case p of Output _ _ -> True
-                                                 Assert _   -> True
-                                                 _          -> False ]
----
-
-data Context = Context { ctxtNetlist    :: Netlist
-                       , ctxtInputType  :: String
-                       , ctxtInputIds   :: [InstId]
-                       , ctxtStateType  :: String
-                       , ctxtStateIds   :: [InstId]
-                       , ctxtStateInits :: [(Integer, InputWidth)]
-                       , ctxtRootNet    :: Net
-                       , ctxtTFunName   :: String
-                       , ctxtCFunName   :: String
-                       , ctxtAssertMsg  :: Maybe String }
-
-mkContext :: Netlist -> Net -> Context
-mkContext nl n = Context { ctxtNetlist    = nl
-                         , ctxtInputType  = inptType
-                         , ctxtInputIds   = inputIds
-                         , ctxtStateType  = stType
-                         , ctxtStateIds   = stateIds
-                         , ctxtStateInits = stateInits
-                         , ctxtRootNet    = n
-                         , ctxtTFunName   = tFun
-                         , ctxtCFunName   = cFun
-                         , ctxtAssertMsg  = msg }
-  where inputIds = [ netInstId n | Just n@Net{netPrim = p} <- elems nl
-                                 , case p of Input _ _ -> True
-                                             _         -> False ]
-        stateElems = [ n | Just n@Net{netPrim = p} <- elems nl
-                         , case p of RegisterEn _ _ -> True
-                                     Register   _ _ -> True
-                                     _              -> False ]
-        stateIds = map netInstId stateElems
-        stateInits = map stateInit stateElems
-        stateInit Net{netPrim=RegisterEn init w} = (init, w)
-        stateInit Net{netPrim=Register   init w} = (init, w)
-        stateInit n = error $ "SMT2 backend error: non state net " ++ show n ++
-                              " encountered where state net was expected"
-        inptType = "Input_" ++ nm
-        stType = "State_" ++ nm
-        tFun = "tFun_" ++ nm
-        cFun = "chain_" ++ tFun
-        (nm, msg) = case netPrim n of
-          Output _      outnm   -> ("output_"++outnm,  Nothing)
-          Assert propmsg -> ("assert_"++propnm, Just propmsg)
-            where propnm = wireName nl propWire
-                  propWire = head . netInputWireIds $ (netInputs n !! 1)
-          _ -> error $ "SMT2 backend error: expected Output or Assert net " ++
-                       "but got " ++ show n
-
-------
-
