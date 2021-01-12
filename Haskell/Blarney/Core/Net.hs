0f8d64b404f07572fde62150ce7c938b1a382f02
gameboo <alexandre.joannou@gmail.com>
2021-01-12 19:42:52 +0000

    Moved topoligical sort of Netlist with the definition of Netlist

--- Haskell/Blarney/Core/Net.hs
+++ Haskell/Blarney/Core/Net.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE PatternSynonyms     #-}
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NoRebindableSyntax  #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 {-|
@@ -23,11 +24,15 @@ module Blarney.Core.Net (
 , netInputWireIds -- Helper function to extract all 'NetInput''s 'WireId's
 , Netlist         -- 'Netlist' type to represent a circuit
 , getNet          -- Extract the 'Net' from a 'Netlist' at the provided 'InstId'
+, topologicalSort -- Topologically sort a 'Netlist'
 ) where
 
 import Prelude
 import Data.Maybe
 import Data.Array
+import Data.STRef
+import Data.Array.ST
+import Control.Monad.ST
 
 import Blarney.Core.Prim
 
@@ -70,3 +75,79 @@ getNet :: Netlist -> InstId -> Net
 getNet nl i = fromMaybe err (nl ! i)
   where err = error $ "Blarney.Core.Net: " ++
                       "access to non existing Net at instId " ++ show i
+
+-- topological stort of a netlist
+--------------------------------------------------------------------------------
+
+-- | the 'Mark' type is only useful to the 'topologicalSort' function and should
+--   not be exported
+data Mark = Unmarked | Temporary | Permanent
+-- | get a topologically sorted '[InstId]' for the given 'Netlist'
+topologicalSort :: Netlist -> [InstId]
+topologicalSort nl = runST do
+  -- initialise state for the topological sorting
+  visited <- newArray (bounds nl) Unmarked -- track visit through the netlist
+  sorted  <- newSTRef [] -- sorted list as a result
+  roots  <- newSTRef relevantRoots -- queue of roots to explore next
+  -- run the internal topological sort while there are roots to explore
+  whileM_ (notEmpty roots) do
+    root <- pop roots -- consume a root
+    topoSort visited sorted roots root -- explore from the consumed root
+  -- return the sorted list of InstId
+  reverse <$> readSTRef sorted
+  -- helpers
+  where
+    -- identify Netlist's outputs and state holding elements
+    relevantRoots = [ netInstId n | Just n@Net{netPrim = p} <- elems nl
+                                  , case p of RegisterEn _ _ -> True
+                                              Register   _ _ -> True
+                                              Output     _ _ -> True
+                                              Assert     _   -> True
+                                              _              -> False ]
+
+    whileM_ :: Monad m => m Bool -> m a -> m ()
+    whileM_ pred act = pred >>= \x -> if x then act >> whileM_ pred act
+                                           else return ()
+    -- identify leaf net
+    isLeaf :: Net -> Bool
+    isLeaf Net{ netPrim = Input      _ _ } = True
+    isLeaf Net{ netPrim = RegisterEn _ _ } = True
+    isLeaf Net{ netPrim = Register   _ _ } = True
+    isLeaf _                               = False
+    -- helpers to use an STRef [a] as a stack
+    push stck elem = modifySTRef' stck $ \xs -> elem : xs
+    pushN stck elems = modifySTRef' stck $ \xs -> elems ++ xs
+    pop stck = do top <- head <$> readSTRef stck
+                  modifySTRef' stck $ \xs -> tail xs
+                  return top
+    notEmpty stck = do l <- readSTRef stck
+                       return (not . null $ l)
+    -- the actual recursive topological sort algorithm
+    topoSort :: STArray s InstId Mark -> STRef s [InstId] -> STRef s [InstId]
+             -> InstId
+             -> ST s ()
+    topoSort visited sorted roots netId = do
+      let net = getNet nl netId
+      -- retrieve the 'visited' array entry for the current net
+      netVisit <- readArray visited netId
+      case netVisit of
+        -- For already visited nets, do not do anything
+        Permanent -> return ()
+        -- For nets under visit, we identified a combinational cycle
+        -- (unsupported ==> error out)
+        Temporary -> error $ "Blarney.Core.Net: " ++
+                             "combinational cycle detected -- " ++ show net
+        -- For new visits:
+        Unmarked -> do
+          let allInputIds = concatMap (map fst . netInputWireIds)
+                                      (netInputs net)
+          -- For leaf nets, strop here and mark inputs as roots for next
+          -- toplevel call
+          if isLeaf net then do pushN roots allInputIds
+          -- For non leaf nets, mark net as temporarily under visit and explore
+          -- all inputs recursively
+          else do writeArray visited netId Temporary
+                  mapM_ (topoSort visited sorted roots) allInputIds
+          -- Mark net as permanent and insert it into the sorted list
+          writeArray visited netId Permanent
+          push sorted netId
