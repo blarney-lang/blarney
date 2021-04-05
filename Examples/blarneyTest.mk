5b99361f87d8c1d7d2750d996adef6fe5fa4e3f0
gameboo <alexandre.joannou@gmail.com>
2021-04-05 12:03:42 +0100

    Cleaner Makefile for examples, with build files out of source tree

--- Examples/blarneyTest.mk
+++ Examples/blarneyTest.mk
@@ -1,3 +1,11 @@
+ifndef BLARNEY_ROOT
+$(error "the BLARNEY_ROOT environment variable must be set")
+endif
+
+BUILD_DIR = $(BLARNEY_ROOT)/build
+O_DIR = $(BUILD_DIR)/odir
+HI_DIR = $(BUILD_DIR)/hidir
+
 GENS = $(basename $(SRCS))
 
 all: $(GENS)
@@ -10,12 +18,15 @@ all: $(GENS)
 	./$< --simulate
 
 %: %.hs
-	blc $(BLC_FLAGS) $<
+	blc -hidir $(HI_DIR) -odir $(O_DIR) $(BLC_FLAGS) $<
 
 .SECONDARY: $(addsuffix -Verilog, $(GEN))
 %-Verilog: %
 	./$< --verilog
 
-.PHONY: clean
+.PHONY: clean mrproper
 clean:
-	rm -rf *.o *.hi $(GENS) *Verilog
+	rm -rf $(GENS) *Verilog
+
+mrproper: clean
+	rm -rf $(BUILD_DIR)
