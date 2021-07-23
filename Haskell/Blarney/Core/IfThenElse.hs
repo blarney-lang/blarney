c97b9e360e8c9a6df7a1cd2e0f3672db39e77880
Matthew Naylor <mn416@cam.ac.uk>
2021-07-23 10:03:08 +0100

    Add priorityIf library function

--- Haskell/Blarney/Core/IfThenElse.hs
+++ Haskell/Blarney/Core/IfThenElse.hs
@@ -5,7 +5,7 @@
 {-|
 Module      : Blarney.Core.IfThenElse
 Description : Overloaded if-then-else
-Copyright   : (c) Matthew Naylor, 2019
+Copyright   : (c) Matthew Naylor, 2019, 2021
               (c) Alexandre Joannou, 2019
 License     : MIT
 Maintainer  : mattfn@gmail.com
@@ -15,10 +15,23 @@ module Blarney.Core.IfThenElse where
 
 import Prelude
 
--- |Overloaded if-then-else
+-- | Overloaded if-then-else
 class IfThenElse b a where
   ifThenElse :: b -> a -> a -> a
 
 instance IfThenElse Bool a where
   ifThenElse False a b = b
   ifThenElse True a b = a
+
+-- | If-then-else chain
+priorityIf :: IfThenElse cond ret => [(cond, ret)] -> ret
+priorityIf [] = error "Blarney.Core.IfThenElse: priorityIf applied to []"
+priorityIf ((a, b):rest) 
+  | null rest = b
+  | otherwise = if a then b else priorityIf rest
+
+-- | If-then-else chain, with fallthrough case
+priorityIfWithFallthrough :: IfThenElse cond ret => [(cond, ret)] -> ret -> ret
+priorityIfWithFallthrough [] def = def
+priorityIfWithFallthrough ((a, b):rest) def =
+  if a then b else priorityIfWithFallthrough rest def
